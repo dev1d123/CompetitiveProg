6e80db3a24473a312770d065a46f9456c0a91f53
dev1d123 <139636102+dev1d123@users.noreply.github.com>
2024-09-04 23:04:24 -0500

    Codemania/17

---
+++ codeforces/div3/CodeforcesRound929/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t; cin>>t;
+	while(t--){
+		int n; cin>>n;
+		int sum=0;
+		while(n--){
+			int value; cin>>value;
+			sum+=abs(value);
+		}
+		cout<<sum<<endl;
+	}
+	
+	return 0;
+}
