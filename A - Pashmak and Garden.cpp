cb73b12125ccc441cd46de7646c6d42210a938d9
dev1d123 <139636102+dev1d123@users.noreply.github.com>
2024-09-11 19:29:55 -0500

    Codemania/30

---
+++ A - Pashmak and Garden.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int x1,y1, x2, y2;
+	cin>>x1>>y1>>x2>>y2;
+	if(x1==x2) cout<<x1+abs(y2-y1)<<" "<<y1<<" "<<x2+abs(y2-y1)<<" "<<y2<<endl;
+	
+	else if(y1 == y2) cout<<x1<<" "<<abs(x1-x2) + y1<<" "<<x2<<" "<<y1+abs(x1-x2)<<endl;
+	
+	else if(abs(x1-x2) == abs(y1-y2)) cout<<x1<<" "<<y2<<" "<<x2<<" "<<y1<<endl;
+	
+	else cout<<-1<<endl;
+	
+	
+	return 0;
+}
