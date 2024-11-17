ba0a4e090910b4d624c3d84e2a14cb0959e03014
dev1d123 <139636102+dev1d123@users.noreply.github.com>
2024-11-17 08:34:39 -0500

    my first interactive prob

---
+++ codeforces/1. Guess the Number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int l = 1, r = 1000000;
+	while(l != r){
+		int mid = (l + r + 1)/2;
+		cout<<mid<<'\n';
+		cout<<flush;
+		string response;
+		cin>>response;
+		if(response == "<"){
+			r = mid - 1;
+		}else{
+			l = mid;
+		}
+	}
+	cout<<"! "<<l<<'\n';
+	cout<<flush;
+	return 0;
+}
